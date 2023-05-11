31ccf38a20531ab6d8bba97253ae80bc7bdadb6d
SenotovaYulia <84642753+SenotovaYulia@users.noreply.github.com>
2023-05-12 01:36:34 +0300

    Add files via upload

---
+++ Lab2/Lab2/RawData.cs
@@ -0,0 +1,227 @@
+﻿//using Lab3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+
+namespace ClassLibrary1
+{
+    public delegate double FRaw(double x);
+    public enum FRawEnum { Linear, Cubic, Random };
+    public class RawData
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public int notes_number { get; set; }
+        public bool is_uniform { get; set; }
+        public FRaw function;
+        public FRawEnum function_name;
+        public double[] grid_notes { get; set; }
+        public double[] grid_values { get; set; }
+        public List<RawDataItem> rawdata_collection { get; set; }
+        public RawData(double init_a, double init_b, int init_note_number, bool init_is_uni, FRawEnum init_function)
+        {
+            a = init_a;
+            b = init_b;
+            notes_number = init_note_number;
+            is_uniform = init_is_uni;
+            function_name = init_function;
+            if(function_name == FRawEnum.Linear)
+            {
+                function = linear;
+            }else if(function_name == FRawEnum.Cubic)
+            {
+                function = cubic;
+            }else if(function_name == FRawEnum.Random)
+            {
+                function = random_function;
+            }
+            grid_notes = new double[notes_number];
+            grid_values = new double[notes_number];
+            if (is_uniform)
+            {
+                double step = (b - a) / (notes_number - 1);
+                for (int i = 0; i < notes_number; i++)
+                {
+                    grid_notes[i] = step * i + a;
+                    grid_values[i] = function(grid_notes[i]);
+                    //Console.Write("In rawdata constructor:");
+                    //Console.Write(grid_notes[i]);
+                    //Console.WriteLine(grid_values[i]);
+                }
+            }
+            else
+            {
+                double step = 1.0 / (notes_number - 1);
+                Console.WriteLine($"step: {step}");
+                for (int i = 0; i < notes_number; i++)
+                {
+                    double x = i * step;
+                    Console.WriteLine($"x: {x}");
+                    grid_notes[i] = a + (b - a) * x * x;
+                    grid_values[i] = function(grid_notes[i]);
+                }
+                /*double x = i * step;
+                        grid_notes[i] = x * x * (b - a) + a;
+                        grid_values[i] = function(grid_notes[i]);
+                        RawDataItem rawdataitem = new RawDataItem(grid_notes[i], grid_values[i]);
+                        rawdata_collection.Add(rawdataitem);*/
+            }
+            rawdata_collection = new List<RawDataItem>();
+            for (int i = 0; i < notes_number; ++i)
+            {
+                RawDataItem rawdataitem = new RawDataItem(grid_notes[i], grid_values[i]);
+                rawdata_collection.Add(rawdataitem);
+            }
+        }
+        public RawData(string filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.Open);
+                BinaryReader binaryReader = new BinaryReader(fs);
+                a = binaryReader.ReadDouble();
+                b = binaryReader.ReadDouble();
+                notes_number = binaryReader.ReadInt32();
+                is_uniform = binaryReader.ReadBoolean();
+                int temp = binaryReader.ReadInt32();
+                function_name = (FRawEnum)temp;
+                if (function_name == FRawEnum.Linear)
+                {
+                    function = linear;
+                }
+                else if (function_name == FRawEnum.Cubic)
+                {
+                    function = cubic;
+                }
+                else if (function_name == FRawEnum.Random)
+                {
+                    function = random_function;
+                }
+                binaryReader.Close();
+                rawdata_collection = new List<RawDataItem>();
+                /*for (int i = 0; i < notes_number; ++i)
+                {
+                    RawDataItem rawdataitem = new RawDataItem(grid_notes[i], grid_values[i]);
+                    rawdata_collection.Add(rawdataitem);
+                }*/
+                if (is_uniform)
+                {
+                    double step = (b - a) / (notes_number - 1);
+                    for (int i = 0; i < notes_number; ++i)
+                    {
+                        grid_notes[i] = step * i + a;
+                        grid_values[i] = function(grid_notes[i]);
+                        RawDataItem rawdataitem = new RawDataItem(grid_notes[i], grid_values[i]);
+                        rawdata_collection.Add(rawdataitem);
+                    }
+                }
+                else
+                {
+                    double step = 1 / (notes_number - 1);
+                    for (int i = 0; i < notes_number; ++i)
+                    {
+                        double x = i * step;
+                        grid_notes[i] = x * x * (b - a) + a;
+                        grid_values[i] = function(grid_notes[i]);
+                        RawDataItem rawdataitem = new RawDataItem(grid_notes[i], grid_values[i]);
+                        rawdata_collection.Add(rawdataitem);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+        public static double linear(double x)
+        {
+            //return 5 * x - 7.5;
+            return x;
+        }
+        public static double cubic(double x)
+        {
+            //return x * x * x + 2 * x * x - 3 * x;
+            return x * x * (x - 1);
+        }
+        public static double random_function(double x)
+        {
+            var rand = new Random();
+            return rand.NextDouble();
+        }
+        public void Save(string filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.OpenOrCreate);
+                BinaryWriter binaryWriter = new BinaryWriter(fs);
+                binaryWriter.Write(a);
+                binaryWriter.Write(b);
+                binaryWriter.Write(notes_number);
+                binaryWriter.Write(is_uniform);
+                int temp = (int)function_name;
+                binaryWriter.Write(temp);
+                for(int i = 0; i < notes_number; ++i)
+                {
+                    binaryWriter.Write(grid_notes[i]);
+                    binaryWriter.Write(grid_values[i]);
+                }
+                binaryWriter.Close();
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+        public static void Load(string filename, ref RawData rawData)
+        {
+            FileStream fs = null;
+            //rawData.a = 1;
+            //rawData = new RawData(0.0, 1.0, 5, true, FRawEnum.Linear);
+            try
+            {
+                fs = new FileStream(filename, FileMode.Open);
+                BinaryReader binaryReader = new BinaryReader(fs);
+                rawData.a = binaryReader.ReadDouble();
+                rawData.b = binaryReader.ReadDouble();
+                rawData.notes_number = binaryReader.ReadInt32();
+                rawData.is_uniform = binaryReader.ReadBoolean();
+                int init_function_name = binaryReader.ReadInt32();
+                rawData.grid_notes = new double[rawData.notes_number];
+                rawData.grid_values = new double[rawData.notes_number];
+                rawData.rawdata_collection = new List<RawDataItem>();
+                for (int i = 0; i < rawData.notes_number; ++i)
+                {
+                    rawData.grid_notes[i] = binaryReader.ReadDouble();
+                    rawData.grid_values[i] = binaryReader.ReadDouble();
+                    RawDataItem rawdataitem = new RawDataItem(rawData.grid_notes[i], rawData.grid_values[i]);
+                    rawData.rawdata_collection.Add(rawdataitem);
+                }
+                binaryReader.Close();
+                rawData.function_name = (FRawEnum)init_function_name;
+                //FRaw linear_function = linear;
+                //rawData = new RawData(init_a, init_b, init_note_number, init_is_uniform, frawenum);
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        } 
+    }
+}
---
+++ Lab2/Lab2/SplineData.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Math;
+namespace ClassLibrary1
+{
+    public class SplineData
+    {
+        public RawData rawdata { get; set; }
+        public int uniform_grid_number { get; set; }
+        public List<SplineDataItem> spline_collection { get; set; }
+        public double left_der { get; set; }
+        public double right_der { get; set; }
+        public double[] coeff { get; set; }
+        public SplineData(RawData init_raw_data, double left_second_der, double right_second_der, int init_uniform_notes_number)
+        {
+            rawdata = init_raw_data;
+            uniform_grid_number = init_uniform_notes_number;
+            left_der = left_second_der;
+            right_der = right_second_der;
+        }
+        public void Execute_Spline()
+        {
+            /*//для хранения значений 0, 1, 2 производной в узлах новой сетки
+            values = new double[3 * points_amount];
+            //хранение коэффицентов интерполяции
+            double[] scoeffhint = new double[4 * (this.rawData.points.Length - 1)];
+            double[] newNUGrid = new double[points_amount];
+            double delta = 0;
+            if (points_amount != 1)
+            {
+                delta = (right_end - left_end) / (points_amount - 1);
+            }
+            for (int i = 0; i < points_amount; i++)
+            {
+                newNUGrid[i] = left_end + delta * i;
+            }
+            //возвращаемое значение
+            int ret = 101;
+            double[] integrals = new double[1];
+            spline_inter(this.rawData.points.Length, this.rawData.points, this.rawData.values,
+                        new double[2] { left_first_der, right_first_der }, newNUGrid, points_amount,
+                        values, scoeffhint, ref ret, integrals, new double[1] { left_end }, new double[1] { right_end });
+            integral = integrals[0];
+            splineDataItems.Clear();
+            for (int i = 0; i < points_amount; i++)
+            {
+                splineDataItems.Add(new SplineDataItem(newNUGrid[i], values[3 * i], values[3 * i + 1], values[3 * i + 2]));
+            }*/
+            int ret = 1;
+            int note_number = rawdata.notes_number;
+            double[] measures = new double[note_number];
+            for (int i = 0; i < note_number; ++i)
+            {
+                measures[i] = rawdata.grid_values[i];
+            }
+            double[] derivatives = new double[2] { left_der, right_der };
+            double[] new_values = new double[3 * uniform_grid_number];
+            double[] arr_integral = new double[1];
+            arr_integral[0] = 0.0;
+            double[] spline_coeff = new double[4 * (note_number - 1)];
+            coeff = new double[4 * (note_number - 1)];
+            double[] new_grid = new double[uniform_grid_number];
+            for(int i = 0; i < uniform_grid_number; ++i)
+            {
+                new_grid[i] = i * (rawdata.b - rawdata.a) / (uniform_grid_number - 1) + rawdata.a;
+            }
+            GlobalFunction(ref ret, note_number, rawdata.grid_notes, measures, derivatives, uniform_grid_number,
+                new_grid, new_values, new double[1] { rawdata.a }, new double[1] { rawdata.b }, arr_integral, spline_coeff);
+            if (ret == 0)
+            {
+                spline_collection = new List<SplineDataItem>();
+                for (int i = 0; i < uniform_grid_number; i++)
+                {
+                    double temp_note = new_grid[i];
+                    double temp_value = new_values[3 * i];
+                    double temp_first_der = new_values[3 * i + 1];
+                    double temp_second_der = new_values[3 * i + 2];
+                    //Console.WriteLine($"{i}:" + temp_value + " " + temp_first_der + " " + temp_second_der + "\n");
+                    spline_collection.Add(new SplineDataItem(temp_note, temp_value, temp_first_der, temp_second_der));
+                }
+                for (int i = 0; i < 4 * (note_number - 1); ++i)
+                {
+                    coeff[i] = spline_coeff[i];//Math.Pow(Math.Pow(spline_coeff[2 * i], 2) + Math.Pow(spline_coeff[2 * i + 1], 2), 0.5);
+                }
+                integral = arr_integral[0];
+            }
+            /*int ret = 1;
+            int note_number = rawdata.notes_number;
+            double[] measures = new double[2 * note_number];
+            for (int i = 0; i < note_number; ++i)
+            {
+                measures[i] = rawdata.grid_values[i];
+                measures[i + note_number] = 0.0;
+            }
+            double[] derivatives = new double[2] { left_der, right_der };
+            double[] new_values = new double[3 * 2 * uniform_grid_number];
+            double[] integrals = new double[2];
+
+            double[] new_grid = new double[uniform_grid_number];
+            for (int i = 0; i < uniform_grid_number; ++i)
+            {
+                new_grid[i] = i * (rawdata.b - rawdata.a) / (uniform_grid_number - 1) + rawdata.a;
+            }
+            double[] spline_coeff = new double[4 * 2 * (note_number - 1)];
+            coeff = new double[4 * (note_number - 1)];
+            GlobalFunction(ref ret, note_number, rawdata.grid_notes, measures, derivatives, uniform_grid_number, new_grid, new_values,
+                new double[1] { rawdata.a }, new double[1] { rawdata.b }, integrals, spline_coeff);
+            if (ret == 0)
+            {
+                spline_collection = new List<SplineDataItem>();
+                for (int i = 0; i < new_grid.Length; i++)
+                {
+                    double new_measures1 = new_values[3 * i];
+                    double new_measures2 = new_values[3 * i + 3 * new_grid.Length];
+                    double new_derivatives1 = new_values[3 * i + 1];
+                    double new_derivatives2 = new_values[3 * i + 3 * new_grid.Length + 1];
+                    double new_second_der1 = new_values[3 * i + 2];
+                    double new_second_der2 = new_values[3 * i + new_grid.Length + 2];
+                    spline_collection.Add(new SplineDataItem(new_grid[i], new_measures1, new_derivatives1, new_second_der1));
+                }
+                for (int i = 0; i < 4 * (note_number - 1); ++i)
+                {
+                    coeff[i] = Math.Pow(Math.Pow(spline_coeff[2 * i], 2) + Math.Pow(spline_coeff[2 * i + 1], 2), 0.5);
+                }
+                integral = integrals[0];
+                Console.WriteLine("Integrals:");
+                Console.WriteLine(integrals[0].ToString(), ' ', integrals[1]);//this.integral2 = integrals[1];
+            }*/
+            /*
+             derivatives, this.new_grid.Length, this.new_grid, new_values, new double[1] { left_border }, new double[1] { right_border }, integrals, spline_coeff);
+            if (ret == 0)
+            {
+                this.new_measures1 = new double[new_grid.Length];
+                this.new_measures2 = new double[new_grid.Length];
+                this.new_derivatives1 = new double[new_grid.Length];
+                this.new_derivatives2 = new double[new_grid.Length];
+                for (int i = 0; i < new_grid.Length; i++)
+                {
+                    this.new_measures1[i] = new_values[3 * i];
+                    this.new_measures2[i] = new_values[3 * i + 3 * new_grid.Length];
+                    this.new_derivatives1[i] = new_values[3 * i + 1];
+                    this.new_derivatives2[i] = new_values[3 * i + 3 * new_grid.Length + 1];
+                }
+                this.integral1 = integrals[0];
+                this.integral2 = integrals[1];
+            }*/
+        }
+        public double integral { get; set; }
+        [DllImport("C:\\Users\\user\\Documents\\C#_projects\\Solution1\\x64\\Debug\\Dll1.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GlobalFunction(ref int ret, int note_number, double[] notes, double[] measures, double[] derivatives,
+    int new_note_number, double[] new_grid, double[] new_values, double[] left_integ, double[] right_integ, double[] integrals,
+    double[] spline_coeff);
+    }
+}
